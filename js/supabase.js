d8b0333ccc55a75323ee0df4660d3a9137870e43
Rangner21 <rhangnnerc6@gmail.com>
2026-04-16 08:32:33 -0300

    Add files via upload

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,7 @@
+const SUPABASE_URL = "https://lafpxxoyjuqtvwiqzoci.supabase.co";
+const SUPABASE_KEY = "sb_publishable_AYcJoBpQTHM39IhvfnBuQg_BKC5rLNy";
+
+const { createClient } = supabase;
+
+window.supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY);
 
