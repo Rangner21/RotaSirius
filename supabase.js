d42115edb862066bb04643139153535e8652eeea
Rangner21 <rhangnnerc6@gmail.com>
2026-04-16 08:31:57 -0300

    Delete supabase.js

--- supabase.js
+++
@@ -1,7 +0,0 @@
-const SUPABASE_URL = "https://lafpxxoyjuqtvwiqzoci.supabase.co";
-const SUPABASE_KEY = "sb_publishable_AYcJoBpQTHM39IhvfnBuQg_BKC5rLNy";
-
-const { createClient } = supabase;
-
-window.supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY);
-
